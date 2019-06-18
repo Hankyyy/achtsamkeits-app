3464c7c5853ee25ea25eeaa28931afb017441ad9
JakobMick <jakobmick@live.de>
2019-06-19 01:01:31 +0200

    ZielScreen
    Sehr frühe Version

--- lib/ZieleScreen/ZieleScreen.dart
+++ lib/ZieleScreen/ZieleScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:aaproto2/ThemeData.dart';
+import 'package:aaproto2/ZieleScreen/ZielCardWidget.dart';
 
 class ZieleScreen extends StatefulWidget {
   @override
@@ -7,11 +9,45 @@ class ZieleScreen extends StatefulWidget {
 }
 
 class _ZieleScreenState extends State<ZieleScreen> {
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Text("Ziele"),
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 40.0, left: 15.0, bottom: 10.0),
+            child: Text(
+              "Ziele",
+              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ZielCard(
+            1,
+            1,
+            2,
+            5,
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 10.0),
+          ),
+          ZielCard(
+            2,
+            1,
+            2,
+            1,
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 10.0),
+          ),
+          ZielCard(
+            3,
+            4,
+            6,
+            8,
+          ),
+        ],
+      ),
     );
   }
 }
