35a2475d35d8c46123b4953c866b1b6ae86e79b0
Hankyyy <10.96@gmx.de>
2019-09-13 08:30:29 +0200

    datenbank models

---
+++ lib/Model/produktivitaet.dart
@@ -0,0 +1,42 @@
+import 'package:aaproto2/Database/dbHelper.dart';
+import 'package:sqflite/sqflite.dart';
+
+class Produktivitaet {
+  //int id;
+  int pWert;
+  String datum;
+
+  Produktivitaet({
+    //this.id,
+    this.pWert,
+    this.datum
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'gWert': pWert,
+      'datum': datum,
+    };
+  }
+
+  Future<void> insertProduktivitaet(Produktivitaet p) async {
+    final Database db = await DB.instance.initDB();
+    await db.insert(
+      'produktivitaet',
+      p.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  //liefert alle Einträge
+  Future<List<Produktivitaet>> msget() async {
+    final Database db = await DB.instance.initDB();
+    final List<Map<String, dynamic>> maps = await db.query('produktivitaet');
+    return List.generate(maps.length, (i) {
+      return Produktivitaet(
+        pWert: maps[i]['pWert'],
+        datum: maps[i]['datum'],
+      );
+    });
+  }
+}
